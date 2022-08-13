4f82445e85d4c928c296779ae230cb4745302719
jnfdev <jairo.jnf.2010@gmail.com>
2022-08-13 11:35:10 -0500

    Validations before init plugin.

--- am-apiplugin.php
+++ am-apiplugin.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Plugin Name: Am API Based Plugin
+ * Description: This plugin is an application test (code challenge) for Awesome Motive.
  * Author: jnfdev
  * Domain: am-apiplugin
  * Domain Path: /languages
@@ -9,7 +10,9 @@ namespace Am\APIPlugin;
 
 defined( 'ABSPATH' ) || exit;
 
-require_once __DIR__.'/vendor/autoload.php';
+if ( ! class_exists( APIPlugin::class ) && is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+    require_once __DIR__.'/vendor/autoload.php';
+}
 
 /**
  * Define consts
@@ -19,4 +22,13 @@ define( 'JNFDEV_APIPLUGIN_ROOT_PATH', __DIR__ );
 define( 'JNFDEV_APIPLUGIN_ROOT_URL', plugin_dir_url( __FILE__ ) );
 define( 'JNFDEV_APIPLUGIN_TEXTDOMAIN', 'jnfdev-apiplugin' );
 
-APIPlugin::run();
+function am_apiplugin_init() {
+    if ( ! class_exists( APIPlugin::class ) ) {
+        error_log('Class APIPlugin not found. Did you forget to run composer install?');
+        return;
+    }
+
+    APIPlugin::run();
+}
+
+am_apiplugin_init();
