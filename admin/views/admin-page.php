6c967e212d9864e32d8e94e52bbc5b500af0e3a2
jnfdev <jairo.jnf.2010@gmail.com>
2022-11-28 13:35:44 -0500

    Noscript error handeling.

--- admin/views/admin-page.php
+++ admin/views/admin-page.php
@@ -1,1 +1,17 @@
 <div id="am-vue-app"></div>
+
+<noscript>
+    <style type="text/css">
+        #am-vue-app{ display:none; }
+        .noscriptmsg {
+            font-weight: bold;
+            font-size: 2rem;
+            padding: 3rem 15px 0 15px;
+            line-height: 1.5;
+        }
+    </style>
+    <div class="noscriptmsg">
+        You don't have javascript enabled. <br> 
+        Please enable it to use this functionality.
+    </div>
+</noscript>
