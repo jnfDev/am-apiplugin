012d5097daa70afe91d84807585f14aae14f1b74
jnfdev <jairo.jnf.2010@gmail.com>
2022-11-30 10:27:20 -0500

    Table component: - Basic structure with settings integration. - Date component to handle humandate logic.

---
+++ src/components/Date.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({ 
+    timestamp: {
+        type: Number,
+        required: true
+    }, 
+    humandate: {
+        type: Boolean,
+        default: true
+    } 
+})
+
+const date = computed(() => {
+    const { humandate, timestamp } = props
+
+    if (!humandate) {
+        return timestamp
+    }
+
+    return new Date(timestamp * 1000).toLocaleDateString()
+} )
+
+</script>
+
+<template>
+    {{ date }}
+</template>
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,3 +1,52 @@
+<script setup>
+    
+    import { storeToRefs } from 'pinia'
+    import { useDataStore } from '../stores/data'
+    import { useSettingStore } from '../stores/settings'
+
+    import Date from '../components/Date.vue'
+
+    const dataStore = useDataStore()
+    const settingStore = useSettingStore()
+
+    const { numrows, humandate, emails } = storeToRefs(settingStore)
+    const { table } = storeToRefs(dataStore)
+</script>
+
 <template>
-    <h1>Table</h1>
+    <h1>{{ table.title }}</h1>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="col in table.headers" :key="col">
+                    {{ col }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in table.rows" :key="row.id">
+                <td>
+                    {{row.id}}
+                </td>
+                <td>
+                    {{ row.url }}
+                </td>
+                <td>
+                    {{ row.title }}
+                </td>
+                <td>
+                    {{ row.pageviews }}
+                </td>
+                <td>
+                    <Date :timestamp="row.date" :humandate="humandate" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <ul>
+        <li v-for="email in emails" :key="email">
+            {{ email }}
+        </li>
+    </ul>
 </template>
